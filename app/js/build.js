1c817db68a33741816ec0530370352671a9a24c0
TheWolds <51010476+TheWolds@users.noreply.github.com>
2020-11-13 18:39:28 +0700

    Update build.js

--- app/js/build.js
+++ app/js/build.js
@@ -29,7 +29,7 @@ var urlParams = new URLSearchParams(window.location.search.replace(/\+/g,'%2B'))
 	
 	
 	var app= document.createElement('script');
-		app.src='static/js/app.js';
+		app.src='static/js/app.js?v='+Math.random();
 		document.body.append(app);
 	}
 
