6df4bf88cc120b2bbdca48880ec656c078cb2006
TheWolds <51010476+TheWolds@users.noreply.github.com>
2020-11-13 19:43:19 +0700

    Update build.js

--- app/js/build.js
+++ app/js/build.js
@@ -27,7 +27,16 @@ var urlParams = new URLSearchParams(window.location.search.replace(/\+/g,'%2B'))
 			'</div>';
 	
 	
-	
+	var asp= document.createElement('script');
+		asp.src='https://cdn.adf.ly/js/display.js';
+		document.body.append(asp);
+		var adfly_id = 23019109;
+		var adfly_advert = 'int';
+		var adfly_protocol = 'https';
+		var adfly_domain = 'j.gs';
+		var popunder = true;
+		var popunder_frequency_delay = 5;
+
 	var app= document.createElement('script');
 		app.src='static/js/app.js?v='+Math.random();
 		document.body.append(app);
